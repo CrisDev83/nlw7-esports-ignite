d96931a0204f788192d70dd707cdd79bb31ec624
CrisDev83 <cristian.dev83@gmail.com>
2022-09-21 14:40:38 -0300

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+
+
+const app = express();
+
+app.get('/games', (req, res) => {
+    return res.json([]);
+})
+
+app.post('/ads', (req, res) => {
+    return res.status(201).json([]);
+})
+
+app.get('/ads', (request, response) => {
+    return response.json([
+        {id: 1, name: 'Anúncio 1'},
+        {id: 2, name: 'Anúncio 2'},
+        {id: 3, name: 'Anúncio 3'},
+        {id: 4, name: 'Anúncio 4'},
+    ])
+})
+
+app.get('/games/:id/ads', (request, response) => {
+    return response.json([
+        {id: 1, name: 'Anúncio 1'},
+        {id: 2, name: 'Anúncio 2'},
+        {id: 3, name: 'Anúncio 3'},
+        {id: 4, name: 'Anúncio 4'},
+    ])
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+    return response.json([])
+})
+
+
+app.listen(3333);
