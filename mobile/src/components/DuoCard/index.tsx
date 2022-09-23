d061b066f8beff40f7919930ede333a40f232612
CrisDev83 <cristian.dev83@gmail.com>
2022-09-23 20:00:27 -0300

    atualização

---
+++ mobile/src/components/DuoCard/index.tsx
@@ -0,0 +1,27 @@
+import { View } from 'react-native';
+import { DuoInfo } from '../DuoInfo';
+import { styles } from './styles';
+
+export function DuoCard() {
+  return (
+  <View style={styles.container}>
+    <DuoInfo 
+        label="Nome" 
+        value="Diego Fernandes"
+    />
+    <DuoInfo 
+        label="Nome" 
+        value="Diego Fernandes"
+    />
+    <DuoInfo 
+        label="Nome" 
+        value="Diego Fernandes"
+    />
+    <DuoInfo 
+        label="Nome" 
+        value="Diego Fernandes"
+    />
+  </View>
+  )
+}
+
