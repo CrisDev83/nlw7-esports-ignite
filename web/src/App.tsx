4b2fd11dfc325a123a8c60fbf3da1f1485a40761
CrisDev83 <cristhian.dev83@gmail.com>
2022-12-14 20:10:48 -0300

    mandar pro github

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -35,12 +35,12 @@ function App() {
   }, [])
 
   return (
-    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+    <div className="max-w-[900px] mx-auto flex flex-col items-center my-10">
       <img src={logoImg} alt="" />
 
-      <h1 className="text-6xl text-white font-black mt-20">Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui.</h1>
+      <h1 className="text-6xl text-white font-black mt-16">Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui.</h1>
 
-      <div className="grid grid-cols-6 gap-6 mt-16">
+      <div className="grid grid-cols-6 gap-6 mt-10">
         {games.map(game => {
           return (
           <GameBanner
